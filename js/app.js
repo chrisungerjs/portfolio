c4655d00551dc693d9fc3f6d682f333ce3a62df2
christian <ceunger91@gmail.com>
2020-08-15 01:09:47 -0400

    add mobile styles, favicon, changed dark mode image

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const themeButton = document.getElementById('theme-button')
 const themeComponents = document.querySelectorAll('.theme')
+const themeIcon = document.querySelector('.fa-lightbulb')
 
 const handleTheme = e => {
+    const iconClasses = ['far', 'fa-lightbulb', 'fas', 'fa-moon']
+    iconClasses.forEach(className => themeIcon.classList.toggle(className))
     themeComponents.forEach(component => component.classList.toggle('DARK'))
 }
 themeButton.addEventListener('click', handleTheme, false)
+
+// dark mode by default
+themeComponents.forEach(component => component.classList.toggle('DARK'))
